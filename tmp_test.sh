d15946db7aec9e08a0070615b7241fb5c5c0207d
Bruce <bruce.belson@my.jcu.edu.au>
2020-06-01 18:54:14 +1000

    Test shells

---
+++ tmp_test.sh
@@ -0,0 +1,3 @@
+python train.py --model_name cnn_1d --data_name CWRU      --normalise_type mean-std --processing_type R_A --max_epoch 10 --data_dir /vann/data/zhao/cwru --batch_size 16
+python train.py --model_name cnn_2d --data_name CWRUCWT   --normalise_type mean-std --processing_type R_A --max_epoch 10 --data_dir /vann/data/zhao/cwru --batch_size 16
+python train.py --model_name cnn_2d --data_name CWRUSlice --normalise_type mean-std --processing_type R_A --max_epoch 10 --data_dir /vann/data/zhao/cwru --batch_size 16
